90521589aac7224aa19f9bfc369ebb6ad5476b4b
DvTox <49532943+DvTox@users.noreply.github.com>
2024-06-22 19:37:04 +0200

    Format using Prettier

--- scripts/module.js
+++ scripts/module.js
@@ -1,36 +1,40 @@
 function registerSettingsMenu() {
-    game.settings.register('tailwind-injector', 'disablePreflight', {
-        name: 'Disable Preflight',
-        hint: 'Whether Preflight should be Disabled(True) or Enabled(False)',
-        scope: 'world',     // "world" = sync to db, "client" = local storage
-        config: true,       // false if you don't want it to show in module config
-        type: Boolean,       // Use the Boolean constructor for type definition
-        default: true,
-        onChange: value => { // value is the new value of the setting
-            console.log(value);
-        },
-        requiresReload: true, // true if you want to prompt the user to reload
-    });
+  game.settings.register("tailwind-injector", "disablePreflight", {
+    name: "Disable Preflight",
+    hint: "Whether Preflight should be Disabled(True) or Enabled(False)",
+    scope: "world", // "world" = sync to db, "client" = local storage
+    config: true, // false if you don't want it to show in module config
+    type: Boolean, // Use the Boolean constructor for type definition
+    default: true,
+    onChange: (value) => {
+      // value is the new value of the setting
+      console.log(value);
+    },
+    requiresReload: true, // true if you want to prompt the user to reload
+  });
 }
 
 function injectTailwindCSS() {
-    let tailwind = document.createElement('script');
-    tailwind.src = "https://cdn.tailwindcss.com";
+  let tailwind = document.createElement("script");
+  tailwind.src = "https://cdn.tailwindcss.com";
 
-    document.head.appendChild(tailwind);
+  document.head.appendChild(tailwind);
 }
 
-Hooks.once('init', async function() {
-    registerSettingsMenu();
-    injectTailwindCSS();
+Hooks.once("init", async function () {
+  registerSettingsMenu();
+  injectTailwindCSS();
 });
 
-Hooks.once('ready', async function() {
-    const disablePreflight = game.settings.get('tailwind-injector', 'disablePreflight');
+Hooks.once("ready", async function () {
+  const disablePreflight = game.settings.get(
+    "tailwind-injector",
+    "disablePreflight"
+  );
 
-    if (disablePreflight) {
-        tailwind.config = { corePlugins: { preflight: false } };
-    } else {
-        tailwind.config = { corePlugins: { preflight: true } };
-    }
+  if (disablePreflight) {
+    tailwind.config = { corePlugins: { preflight: false } };
+  } else {
+    tailwind.config = { corePlugins: { preflight: true } };
+  }
 });
