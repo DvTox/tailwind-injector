e0866045fa22e85ac10129b9b03e34b681bc2316
DvTox <49532943+DvTox@users.noreply.github.com>
2024-06-22 18:42:31 +0200

    Implement Settings Menu

--- scripts/module.js
+++ scripts/module.js
@@ -1,3 +1,18 @@
+function registerSettingsMenu() {
+    game.settings.register('tailwind-injector', 'disablePreflight', {
+        name: 'Disable Preflight',
+        hint: 'Wheter Preflight should be Disabled(True) or Enabled(False)',
+        scope: 'world',     // "world" = sync to db, "client" = local storage
+        config: true,       // false if you dont want it to show in module config
+        type: Boolean,       // You want the primitive class, e.g. Number, not the name of the class as a string
+        default: true,
+        onChange: value => { // value is the new value of the setting
+          console.log(value)
+        },
+        requiresReload: true, // true if you want to prompt the user to reload
+    });
+}
+
 function injectTailwindCSS() {
     let Tailwind = document.createElement('script');
     Tailwind.src = "https://cdn.tailwindcss.com"
@@ -6,11 +21,16 @@ function injectTailwindCSS() {
 }
 
 Hooks.once('init', async function() {
-    console.log("Injecting Tailwind CSS");
+    registerSettingsMenu()
     injectTailwindCSS()
-    alert("Injected Tailwind CSS")
 });
 
 Hooks.once('ready', async function() {
-    tailwind.config = {corePlugins: {preflight: false}};
+    const disablePreflight = game.settings.get('tailwind-injector', 'disablePreflight');
+
+    if (disablePreflight) {
+        tailwind.config = {corePlugins: {preflight: false}};
+    }else{
+        tailwind.config = {corePlugins: {preflight: true}};
+    }
 });
