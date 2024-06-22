a8000ffb47dbc9973bcbba659a37aa36c3e91b1e
DvTox <49532943+DvTox@users.noreply.github.com>
2024-06-22 19:33:06 +0200

    Fix Spelling / Formating

--- scripts/module.js
+++ scripts/module.js
@@ -1,36 +1,36 @@
 function registerSettingsMenu() {
     game.settings.register('tailwind-injector', 'disablePreflight', {
         name: 'Disable Preflight',
-        hint: 'Wheter Preflight should be Disabled(True) or Enabled(False)',
+        hint: 'Whether Preflight should be Disabled(True) or Enabled(False)',
         scope: 'world',     // "world" = sync to db, "client" = local storage
-        config: true,       // false if you dont want it to show in module config
-        type: Boolean,       // You want the primitive class, e.g. Number, not the name of the class as a string
+        config: true,       // false if you don't want it to show in module config
+        type: Boolean,       // Use the Boolean constructor for type definition
         default: true,
         onChange: value => { // value is the new value of the setting
-          console.log(value)
+            console.log(value);
         },
         requiresReload: true, // true if you want to prompt the user to reload
     });
 }
 
 function injectTailwindCSS() {
-    let Tailwind = document.createElement('script');
-    Tailwind.src = "https://cdn.tailwindcss.com"
+    let tailwind = document.createElement('script');
+    tailwind.src = "https://cdn.tailwindcss.com";
 
-    document.head.appendChild(Tailwind);
+    document.head.appendChild(tailwind);
 }
 
 Hooks.once('init', async function() {
-    registerSettingsMenu()
-    injectTailwindCSS()
+    registerSettingsMenu();
+    injectTailwindCSS();
 });
 
 Hooks.once('ready', async function() {
     const disablePreflight = game.settings.get('tailwind-injector', 'disablePreflight');
 
     if (disablePreflight) {
-        tailwind.config = {corePlugins: {preflight: false}};
-    }else{
-        tailwind.config = {corePlugins: {preflight: true}};
+        tailwind.config = { corePlugins: { preflight: false } };
+    } else {
+        tailwind.config = { corePlugins: { preflight: true } };
     }
 });
